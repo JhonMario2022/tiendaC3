ac8c727b4aa294742999f27a4f0cca06abd1c0e3
Jhon Mario <jhomadel@yahoo.es>
2022-11-30 21:04:21 -0500

    Initial commit

---
+++ TiendaNivC3/src/main/java/com/example/demo/TiendaC3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaC3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaC3Application.class, args);
+	}
+
+}
